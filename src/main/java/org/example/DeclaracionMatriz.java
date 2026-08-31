b0c9c11a46d1b07c1b9b7f40d1a340f146ac0898
mapeflo <mpenarandaf@unicartagena.edu.co>
2026-08-30 23:07:47 -0500

    feat(matrices-declaracion): crear matriz 3x3 e inicializarla con valores del 1 al 9

---
+++ src/main/java/org/example/DeclaracionMatriz.java
@@ -0,0 +1,23 @@
+package org.example;
+
+public class DeclaracionMatriz {
+
+    public static void main(String[] args) {
+
+        int[][] matriz = {
+                {1, 2, 3},
+                {4, 5, 6},
+                {7, 8, 9}
+        };
+
+        System.out.println("Matriz 3x3 inicializada con valores del 1 al 9:");
+        System.out.println();
+
+        for (int i = 0; i < matriz.length; i++) {
+            for (int j = 0; j < matriz[i].length; j++) {
+                System.out.print(matriz[i][j] + "\t");
+            }
+            System.out.println();
+        }
+    }
+}
